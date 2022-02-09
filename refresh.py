858e9099a84d042514eeab437d30f77d9abb1378
Justin Barnowski <jbarno@shirlywhirlmd.com>
2022-02-08 20:58:15 -0600

    fix(import): was not importing it correctly

--- refresh.py
+++ refresh.py
@@ -3,14 +3,13 @@ import asyncio
 import fire
 import json
 import os
-import pynacl
 
 # YOINK: https://docs.github.com/en/rest/reference/actions#example-encrypting-a-secret-using-python
 from base64 import b64encode
 from nacl import encoding, public
 
 def _encrypt(public_key: str, secret_value: str) -> str:
-"""Encrypt a Unicode string using the public key."""
+    """Encrypt a Unicode string using the public key."""
     public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
     sealed_box = public.SealedBox(public_key)
     encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
