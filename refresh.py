099fb6c7cb55f0a74b0966c0d92c796a7a5a5870
Justin Barnowski <jbarno54@gmail.com>
2022-05-02 21:46:48 -0500

    fix(await): probably want to have the data before returning

--- refresh.py
+++ refresh.py
@@ -25,8 +25,8 @@ async def _get_github_pub_key(session, org):
                "Authorization": "token " + os.environ["PAT_FOR_PUB"]}
     url = "https://api.github.com/orgs/{org}/actions/secrets/public-key".format(org = org)
     async with session.get(url, headers=headers) as resp:
-        data = resp.json()
-        return await (data["key"], data["key_id"])
+        await data = resp.json()
+        return (data["key"], data["key_id"])
 
 async def _put_github_repo_secret(session, org, repo, key_id, secret_name, encrypted_secret):
     headers = {"accept": "application/vnd.github.v3+json",
