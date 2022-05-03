bb02524ba4a4eab9d88503ef1ce8cbcfd9e9448a
Justin Barnowski <jbarno54@gmail.com>
2022-05-02 21:23:52 -0500

    fix(authorization): hmm how does one rotate tokens when rotating tokens to rotate tokens
    PUB->admin
    
    PUT->repo

--- refresh.py
+++ refresh.py
@@ -21,13 +21,15 @@ async def _get_insta_token(session, url, token):
         return await resp.text()
 
 async def _get_github_pub_key(session, org):
-    headers = {"accept": "application/vnd.github.v3+json"}
+    headers = {"accept": "application/vnd.github.v3+json",
+               "Authorization: token": os.environ["PAT_FOR_PUB"]}
     url = "https://api.github.com/org/{org}/actions/secrets/public-key".format(org = org)
     async with session.get(url, headers=headers) as resp:
         return await (resp.json["key"], resp.json["key_id"])
 
 async def _put_github_repo_secret(session, org, repo, key_id, secret_name, encrypted_secret):
-    headers = {"accept": "application/vnd.github.v3+json"}
+    headers = {"accept": "application/vnd.github.v3+json",
+               "Authorization: token": os.environ["PAT_FOR_PUT"]}
     url = "https://api.github.com/{org}/{repo}/actions/secrets/{secret_name}".format(org = org, repo = repo, secret_name = secret_name)
     data = { "encrypted_value" : encrypted_secret,
              "key_id" : key_id }
